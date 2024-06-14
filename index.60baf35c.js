454984cc37237e20f5db64926c90356370cd9368
Александр <0999353523ak@gmail.com>
2024-06-14 14:07:19 +0300

    make build

---
+++ index.60baf35c.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider"),o=e.getBoundingClientRect(),d=t.getBoundingClientRect(),l=getComputedStyle(e),r=parseFloat(l.borderTopWidth),i=parseFloat(l.borderLeftWidth);t.style.top="".concat(o.height/2-r-d.height/2,"px"),t.style.left="".concat(o.width/2-i-d.width/2,"px");
+//# sourceMappingURL=index.60baf35c.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,13 @@
+"use strict";
+// write code here
+const wall = document.querySelector(".wall");
+const spider = document.querySelector(".spider");
+const wallParams = wall.getBoundingClientRect();
+const spiderParams = spider.getBoundingClientRect();
+const wallStyles = getComputedStyle(wall);
+const borderTop = parseFloat(wallStyles.borderTopWidth);
+const borderLeft = parseFloat(wallStyles.borderLeftWidth);
+spider.style.top = `${wallParams.height / 2 - borderTop - spiderParams.height / 2}px`;
+spider.style.left = `${wallParams.width / 2 - borderLeft - spiderParams.width / 2}px`;
+
+//# sourceMappingURL=index.f75de5e1.js.map
